1aca15b850153cdbb82d0660a2a42b4894db0f39
syauqi <cuktaw1@gmail.com>
2025-07-01 11:57:05 +0700

    commit

--- src/main/java/utilities/DriverManager.java
+++ src/main/java/utilities/DriverManager.java
@@ -2,6 +2,7 @@ package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
@@ -9,15 +10,25 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if(driver == null) {
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new"); // Mode headless Chrome
+            options.addArguments("--window-size=1920,1080"); // Ukuran layar virtual
             //Initialize the WebDriver instance here
             //For example, using ChromeDriver;
 
-            driver = WebDriverManager.chromedriver().create();
-            driver.manage().window().maximize();
-            driver.get("https://demoblaze.com/");
+            driver = WebDriverManager.chromedriver().capabilities(options).create();
+            driver.get("https://demoblaze.com/"); // Perbaiki format URL
         }
         return driver;
     }
+
+//            driver = WebDriverManager.chromedriver().create();
+//            driver.manage().window().maximize();
+//            driver.get("https://demoblaze.com/");
+//        }
+//        return driver;
+//    }
 
     public static void quitDriver(){
         if (driver != null){
